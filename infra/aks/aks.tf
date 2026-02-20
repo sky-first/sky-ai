fdec6aae2a57616e85aa804ce92d95bf4c95c87e
Gustavo Mendonca <gustavo.mendonca@thedatafirst.com>
2026-02-20 09:18:02 +0000

    chore(infra): desidratação de custos e estabilidade pós-teste

--- infra/aks/aks.tf
+++ infra/aks/aks.tf
@@ -68,10 +68,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_kubernetes_cluster_node_pool" "user_pool" {
   name                        = "userapps"
   kubernetes_cluster_id       = azurerm_kubernetes_cluster.aks.id
-  vm_size                     = "Standard_D4s_v3"
+  vm_size                     = "Standard_D2s_v3"
   auto_scaling_enabled        = true
-  min_count                   = 2
-  max_count                   = 10
+  min_count             = 1
+  max_count             = 3
   priority                    = "Regular"
   temporary_name_for_rotation = "userappsrot1"
 
