f9383e1b7ed44499720a3540fbbadb1f9fe43d82
Gustavo Mendonca <gustavo.mendonca@thedatafirst.com>
2026-03-13 10:43:15 +0000

    Removing invalid name field from migration-job.yaml template

--- infra/aks/backups.tf
+++ infra/aks/backups.tf
@@ -16,6 +16,9 @@ resource "azurerm_storage_account" "db_backup" {
   min_tls_version          = "TLS1_2"
 
   # Immutability & Protection
+  is_version_level_immutability_supported = true
+
+  # Immutability & Protection
   blob_properties {
     versioning_enabled       = true
     change_feed_enabled      = true
@@ -42,6 +45,24 @@ resource "azurerm_storage_container" "sql_backups" {
   name                  = "sql-backups"
   storage_account_name  = azurerm_storage_account.db_backup.name
   container_access_type = "private"
+}
+
+# 3.1 Dedicated Container for Config Backups
+resource "azurerm_storage_container" "config_backups" {
+  name                  = "config-backups"
+  storage_account_name  = azurerm_storage_account.db_backup.name
+  container_access_type = "private"
+}
+
+# 3.2 Immutability Policies (WORM)
+resource "azurerm_storage_container_immutability_policy" "sql_immutability" {
+  storage_container_id_resource_manager_id = azurerm_storage_container.sql_backups.resource_manager_id
+  immutability_period_since_creation_in_days = 7
+}
+
+resource "azurerm_storage_container_immutability_policy" "config_immutability" {
+  storage_container_id_resource_manager_id = azurerm_storage_container.config_backups.resource_manager_id
+  immutability_period_since_creation_in_days = 7
 }
 
 # 4. Lifecycle Management Policy
